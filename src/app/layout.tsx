3f6718b43b26135b48c55184bc38d5b7df1fd007
Jeremy <jeremy.teurterie@gmail.com>
2024-01-03 23:26:47 +0700

    resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
     <html lang="en" className={inter.className}>
       <body>{children}</body>
       <Analytics />
+      <Analytics />
     </html>
   );
 }
